c072b6715367a08c30d07437be007b97e40fb9f0
20607206 <s5556380@gmail.com>
2025-04-08 18:03:30 +0800

    new

--- get_price.py
+++ get_price.py
@@ -12,11 +12,17 @@ def get_stock_price(stock_code):
     return f"{stock_name} ({stock_code}) 目前股價：{stock_price}"
 '''
 
+def remove_trailing_zero(number_str):
+    if '.' in number_str:
+        number_str = number_str.rstrip('0').rstrip('.')
+        return number_str
 
 def get_twstock_price(stock_code):
     realtime_data = twstock.realtime.get(stock_code)
 
     if realtime_data and realtime_data['realtime']:
-        return f"股票代碼：{realtime_data['info']['code']}\n公司名稱：{realtime_data['info']['name']}\n最新成交價：{realtime_data['realtime']['latest_trade_price']}\n開盤價：{realtime_data['realtime']['open']}\n最高價：{realtime_data['realtime']['high']}\n最低價：{realtime_data['realtime']['low']}"
+        print(f"股票代碼：{realtime_data['info']['code']}\n公司名稱：{realtime_data['info']['name']}\n最新成交價：{remove_trailing_zero(realtime_data['realtime']['latest_trade_price'])}\n開盤價：{remove_trailing_zero(realtime_data['realtime']['open'])}\n最高價：{remove_trailing_zero(realtime_data['realtime']['high'])}\n最低價：{remove_trailing_zero(realtime_data['realtime']['low'])}")
     else:
-        print(f"無法取得股票代碼{stock_code}")
+        return f"無法取得股票代碼{stock_code}"
+
+get_twstock_price("00881")
