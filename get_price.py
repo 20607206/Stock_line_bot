bed22218fa7bffd1f097e117b06cff32a7d0cbe7
20607206 <s5556380@gmail.com>
2025-04-12 14:02:06 +0800

    add yfinace

--- get_price.py
+++ get_price.py
@@ -1,6 +1,16 @@
+import datetime
+import json
 import requests
+import yfinance
 from bs4 import BeautifulSoup
 import twstock
+
+with open('Taiwan Stock Exchange Popular Stock Code Comparison Table.json', "r", encoding="utf-8") as f:
+    stock_name_map = json.load(f)
+
+def get_stock_name(stock_code):
+    return stock_name_map.get(stock_code, "未知公司")
+
 '''
 def get_stock_price(stock_code):
     url = f"https://tw.stock.yahoo.com/quote/{stock_code}.TW"
@@ -18,9 +28,41 @@ def remove_trailing_zero(number_str):
         return number_str
 
 def get_twstock_price(stock_code):
-    realtime_data = twstock.realtime.get(stock_code)
+    try:
+        realtime_data = twstock.realtime.get(stock_code)
+        if realtime_data and realtime_data['realtime']:
 
-    if realtime_data and realtime_data['realtime']:
-        return f"股票代碼：{realtime_data['info']['code']}\n公司名稱：{realtime_data['info']['name']}\n最新成交價：{remove_trailing_zero(realtime_data['realtime']['latest_trade_price'])}\n開盤價：{remove_trailing_zero(realtime_data['realtime']['open'])}\n最高價：{remove_trailing_zero(realtime_data['realtime']['high'])}\n最低價：{remove_trailing_zero(realtime_data['realtime']['low'])}"
-    else:
-        return f"無法取得股票代碼{stock_code}"
+            info = realtime_data.get('info', {})
+            rt = realtime_data.get('realtime', {})
+            return (
+                f"股票代碼:{info.get('code')}\n"
+                f"公司名稱:{info.get(('name'))}\n"
+                f"最新成交價:{remove_trailing_zero(rt.get('latest_trade_price'))}\n"
+                f"開盤價:{remove_trailing_zero(rt.get('open'))}\n"
+                f"最高價:{remove_trailing_zero(rt.get('high'))}\n"
+                f"最低價:{remove_trailing_zero(rt.get('low'))}"
+            )
+        else:
+            return f"無法取得股票代碼{stock_code}"
+    except Exception as e:
+        return f"主資料源錯誤（twstock 失敗）：{e}"
+
+def get_yfinance_price(stock_code):
+    try:
+        today = datetime.date.today().strftime('%Y-%m-%d')
+        ticker = yfinance.Ticker(f'{stock_code}.TW')
+        df = ticker.history(start = today)
+        if not df.empty:
+            latest = df.iloc[-1]
+            return (
+                f"股票代碼:{stock_code}\n"
+                f"公司名稱:{get_stock_name(stock_code)}\n"
+                f"開盤價:{latest['Open']:.2f}\n"
+                f"收盤價:{latest['Close']:.2f}\n"
+                f"最高價:{latest['High']:.2f}\n"
+                f"最低價:{latest['Low']:.2f}"
+            )
+        else:
+            return f"無法取得股票代碼{stock_code}"
+    except Exception as e:
+        return f"主資料源錯誤（yfinance 失敗）：{e}"
