8bcfec107f3c0b0060187635f2198ea1a6847324
20607206 <s5556380@gmail.com>
2025-05-01 17:52:48 +0800

    new

--- stock_parser.py
+++ stock_parser.py
@@ -199,11 +199,15 @@ class StockJobManager:
             self.df_list = pd.DataFrame()
             for code in self.stock_list:
                 df = self.get_yfinance_price(code, self.period)
+                if df.empty:
+                    logging.warning(f"{code} Data not found, try again in 10 seconds")
+                    time.sleep(10)
+                    df = self.get_yfinance_price(code, self.period)
                 self.df_list = pd.concat([self.df_list, df])
             if self.df_list is not None:
-                logging.info(f"找到資料一共{len(self.df_list)}比")
+                logging.info(f"found data total:{len(self.df_list)}")
             else:
-                logging.error("⚠️ 沒有抓取到資料")
+                logging.error("No data !")
         except Exception as e:
             logging.warning(f"{e}")
 
@@ -211,14 +215,29 @@ class StockJobManager:
         logging.info("Job2 is Working...")
         try:
             if not self.df_list.empty:
-                logging.info("資料儲存中...")
+                logging.info("Data is being stored...")
                 SQL.save_stock_to_mysql(self.df_list)
-                logging.info("儲存完成")
+                logging.info("Storage completed")
                 self.df_list = pd.DataFrame()
             else:
-                logging.error("⚠️ 沒有資料")
+                logging.error("No data !")
         except Exception as e:
             logging.warning(f"{e}", exc_info=True)
+
+    def job3(self):
+        logging.info("Job3 Start clearing stock data...")
+        try:
+            if not self.stock_list:
+                logging.error("No stock symbols to delete")
+            else:
+                for code in self.stock_list:
+                    logging.info(f"Starting remove {code}")
+                    SQL.remove_sql(code)
+                    logging.info(f"Delete code : {code}")
+
+        except Exception as e:
+            logging.warning(f"{e}", exc_info=True)
+
 
     @staticmethod
     def get_yfinance_price(stock_code, period, is_tw=True):
@@ -237,12 +256,19 @@ class StockJobManager:
             return pd.DataFrame()
 
     def start_schedule(self):
-        schedule.every().day.at("14:30:30").do(self.job1)
-        schedule.every().day.at("14:31").do(self.job2)
+        logging.info("Start Job3 ")
+        self.job3()
+        time.sleep(5)
 
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+        logging.info("Start Job1 ")
+        self.job1()
+        time.sleep(5)
+
+        logging.info("Start Job2 ")
+        self.job2()
+        time.sleep(5)
+
+        logging.info("Work completed")
 
 if __name__ == "__main__":
     manager = StockJobManager(stock_list)
